ddd70c617aa2f4155a52646369a1613baeda8b6b
Gülden SÖNMEZ <60888798+Gularss@users.noreply.github.com>
2022-09-13 15:54:03 +0300

    Day 2 Add BusinessRules and Global Exception Handling

--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -1,8 +1,15 @@
+using Application.Services;
+using Core.CrossCuttingConcerns.Exceptions;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddApplicationServices();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,6 +22,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+if (app.Environment.IsProduction())
+    app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
